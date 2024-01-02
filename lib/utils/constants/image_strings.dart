d6025aaa5e9c7c8d52723fee623f370f6795d3c8
Ninad More <ninadyogeshmore@gmail.com>
2024-01-02 18:08:19 +0530

    🌟 Logo Changed

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -3,10 +3,13 @@
 /// This class contains all the App Images in String formats.
 class TImages {
   // -- App Logos
-  static const String darkAppLogo =
-      "assets/logos/t-store-splash-logo-black.png";
-  static const String lightAppLogo =
-      "assets/logos/t-store-splash-logo-white.png";
+  // static const String darkAppLogo =
+  //     "assets/logos/t-store-splash-logo-black.png";
+  // static const String lightAppLogo =
+  //     "assets/logos/t-store-splash-logo-white.png";
+
+  static const String darkAppLogo = "assets/logos/e-com-logo.png";
+  static const String lightAppLogo = "assets/logos/e-com-logo.png";
 
   // -- Social Logos
   static const String google = "assets/logos/google-icon.png";
