f5cc6f43a32e8bf659e42079d335879b35ea0cf5
ninad-moree <ninadyogeshmore@gmail.com>
2024-01-26 20:47:28 +0530

    🗂️Firebase Setup Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:e_commerce/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  // Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((FirebaseApp value) => Get.put(AuthenticationRepository));
+
   runApp(const App());
 }
